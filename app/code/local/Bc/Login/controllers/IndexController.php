38099457f39a8e9993ad04d308e465a5e2f33cd5
Hien Truong <hien.truong@bluecomgroup.com>
2015-10-19 14:39:56 +0700

    update

--- app/code/local/Bc/Login/controllers/IndexController.php
+++ app/code/local/Bc/Login/controllers/IndexController.php
@@ -2,6 +2,21 @@
 class Bc_Login_IndexController extends Mage_Core_Controller_Front_Action
 {
     /**
+     * Default customer account page
+     */
+    public function indexAction()
+    {
+        $this->loadLayout();
+        $this->_initLayoutMessages('customer/session');
+        $this->_initLayoutMessages('catalog/session');
+
+        $this->getLayout()->getBlock('content')->append(
+            $this->getLayout()->createBlock('customer/account_dashboard')
+        );
+        $this->getLayout()->getBlock('head')->setTitle($this->__('My Account'));
+        $this->renderLayout();
+    }
+    /**
      * Get Customer Model
      *
      * @return Mage_Customer_Model_Customer
